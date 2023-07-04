a5033351025976a620693a4d4169bcb960725b86
lastonton <138580872+lastonton@users.noreply.github.com>
2023-07-04 18:24:35 +0700

    create project

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT =process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log(`Server listening on ${PORT}`);
+});
+const bodyParsers = require('body-parser');
+app.use(bodyParsers.json({limit : '10mb'}));
+app.use(bodyParsers.urlencoded({extends: true, limit :'10mb'}));
+
+app.get('/hello',(req,res)=>{
+    res.json({message: 'Hello from server'});
+})
+
+app.use('/api/admin', require('./api/admin.js'));
+
+app.use('/api/customer', require('./api/customer.js'));
